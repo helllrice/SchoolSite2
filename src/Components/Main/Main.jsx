c3cdd1f7bd9bd4e3d632fd89d3475ce5b3873f1a
vasiliy <wizardcatt@gmail.com>
2022-07-23 22:10:40 +0300

    beta-version new schoolsite

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,6 @@
 import {Slider} from "./Slider/Slider";
 import {MainInfo} from "./MainInfo/MainInfo";
 
-
 export const Main = () => {
     return (
         <>
--- src/Components/Main/MainInfo/MainInfo.jsx
+++ src/Components/Main/MainInfo/MainInfo.jsx
@@ -5,17 +5,37 @@ export const MainInfo = () => {
         <>
             <div className="main-info">
                 <div className="main-info-box">
-                    <div className="main-item">
-                        <img src="/images/main-img/main-img1.png" alt="main-img"/>
+                    <div className="main-left">
+                        <div className="main-item">
+                            <img src="/images/main-img/main-img4.jpg" alt="main-img"/>
+                            <div className="main-item-title">
+                                <h2>Benefits of an ISWA education</h2>
+                                <button>Find out more ></button>
+                            </div>
+                        </div>
+                        <div className="main-item">
+                            <img src="/images/main-img/main-img1.png" alt="" className="img-2"/>
+                            <div className="main-item-title">
+                                <h2>Benefits of an ISWA education</h2>
+                                <button>Find out more ></button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="main-item">
-                        <img src="/images/main-img/main-img2.jpg" alt=""/>
-                    </div>
-                    <div className="main-item">
-                        <img src="/images/main-img/main-img3.png" alt=""/>
-                    </div>
-                    <div className="main-item">
-                        <img src="/images/main-img/main-img4.jpg" alt=""/>
+                    <div className="main-right">
+                        <div className="main-item">
+                            <img src="/images/main-img/main-img2.jpg" alt=""/>
+                            <div className="main-item-title">
+                                <h2>Benefits of an ISWA education</h2>
+                                <button>Find out more ></button>
+                            </div>
+                        </div>
+                        <div className="main-item">
+                            <img src="/images/main-img/main-img3.png" alt=""/>
+                            <div className="main-item-title">
+                                <h2>Benefits of an ISWA education</h2>
+                                <button>Find out more ></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
